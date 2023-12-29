abf8f65e048b7560b5bf3995258b011c698df74f
andinh03 <153578154+andinh03@users.noreply.github.com>
2023-12-29 02:48:59 +0000

    Finished the rest of program with placeholder options 1 - 3

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 print("Welcome to Chatbot 101!")
 name = input("Please enter your name: ")
-greeting = input(f"Nice to meet you {name}! How old are you? ")
+age = input(f"Nice to meet you {name}! How old are you? ")
+print(f"Welcome, {name}! Oh, so youthful with your {age} years old! How can I help you? ")
+
+print("")
+
+def options_menu():
+  print("Please choose from the following options:")
+  print("1. Placeholder option 1")
+  print("2. Placeholder option 2")
+  print("3. Placeholder option 3")
+  print("4. Exit the conversation")
+
+def user_selection():
+  number = int(input("Enter the number of your choice: "))
+  if number == 4:
+    print(f"Goodbye, {name}! Have a nice day!")
+
+options_menu()
+user_selection()
