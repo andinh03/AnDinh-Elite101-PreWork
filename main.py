8cc584c5ffaaf09ac528d5293416c8eb42a757ca
andinh03 <153578154+andinh03@users.noreply.github.com>
2024-01-03 01:57:34 +0000

    Debug program with no options for placeholder

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def chatbot():
-  print("Welcome to Chatbot 101!")
-  name = input("Please enter your name: ")
-  age = input(f"Nice to meet you {name}! How old are you? ")
-  print(f"Welcome, {name}! Oh, so youthful with your {age} years old!           How can I help you? ")
 
+print("Welcome to Chatbot 101!")
+name = input("Please enter your name: ")
+age = input(f"Nice to meet you {name}! How old are you? ")
+print(f"Welcome, {name}! Oh, so youthful with your {age} years old! How can I help you? ")
 print("")
 
 def options_menu():
@@ -12,12 +11,16 @@ def options_menu():
   print("2. Placeholder option 2")
   print("3. Placeholder option 3")
   print("4. Exit the conversation")
+  print("")
 
-def user_selection():
+  
+def user_selection():  
   number = int(input("Enter the number of your choice: "))
   if number == 4:
-    print(f"Goodbye, {name}! Have a nice day!")
+      print(f"Goodbye, {name}  Have a nice day!")
+  else:
+      return user_selection()
 
-chatbot()
+
 options_menu()
 user_selection()
